2ac95f86fae8ebb4c7a50bb632a7cc6e1a22afd7
nyambaka <nelsonnyambaka@gmail.com>
2019-01-11 22:43:59 +0300

    [finishes #163155314] a user is able to register

--- app/api/v1/utils/utility.py
+++ app/api/v1/utils/utility.py
@@ -17,6 +17,12 @@ def meet_up_add_breath(meet_up, id_dictionary):
     return meet_up
 
 
+def user_add_breath(user, id_dictionary):
+    if not isinstance(user, dict):
+        return 501
+    user["id"] = id_generator(id_dictionary, "meet_up")
+    return user
+
 
 def question_add_breath(question, id_dictionary):
     if not isinstance(question, dict):
@@ -43,19 +49,31 @@ def reflect_meetup(meet_up):
     copy = meet_up.copy()
     if not isinstance(meet_up, dict):
         return 501
-    meetup_feedback=copy
+    meetup_feedback = copy
     del meetup_feedback["question"]
     del meetup_feedback["rsvp"]
     return meetup_feedback
 
 
-def reflect_vote(meet_up,question_index):
+def reflect_vote(meet_up, question_index):
     copy = meet_up.copy()
     if not isinstance(meet_up, dict):
         return 501
     meetup_feedback = copy
-    meetup_feedback["body"] =meet_up["question"][question_index]["body"]
+    meetup_feedback["body"] = meet_up["question"][question_index]["body"]
     meetup_feedback["vote"] = meet_up["question"][question_index]["vote"]
     del meetup_feedback["question"]
     del meetup_feedback["user"]
     return meetup_feedback
+
+
+def validate_user_login_details(user_details):
+    if not isinstance(user_details, dict):
+        return 506
+    if not "username" in user_details.keys():
+        return 507
+    if not "email" in user_details.keys():
+        return 507
+    if not "password" in user_details.keys():
+        return 508
+    return 0
