f684f6e5072d32bb076b012d86b228b8590beb20
nyambaka <nelsonnyambaka@gmail.com>
2019-02-02 13:07:17 +0300

    modified patch class

--- app/api/v1/controller/abs_patch.py
+++ app/api/v1/controller/abs_patch.py
@@ -1,12 +1,14 @@
 from .get_question import GetQuestion
-class Patch():
 
-    def __init__(self,data):
-        self._controller=GetQuestion(data)
+
+class Patch:
+
+    def __init__(self, data):
+        self._controller = GetQuestion(data)
         self._controller.new_display()
-        self.data=data
+        self.data = data
 
-    def up_vote(self,id):
+    def up_vote(self, id):
         int_id = int(id)
         self._controller.get(int_id)
         for i in self.data:
@@ -15,7 +17,7 @@ class Patch():
                     q["vote"] += 1
         return self
 
-    def down_vote(self,id):
+    def down_vote(self, id):
         int_id = int(id)
         self._controller.get(int_id)
         for i in self.data:
